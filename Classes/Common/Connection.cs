f4488b03ed586a523138983cc3253115fd9bde1a
ADMIN <ADMIN@MSI>
2026-05-07 00:23:06 +0500

    Создание класса для работы с БД. Бартова

---
+++ Classes/Common/Connection.cs
@@ -0,0 +1,29 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino_Bartova.Classes.Common
+{
+    public class Connection
+    {
+        public static readonly string config = "server=10.0.204.1; uid=root; pwd=; database=kino;";
+        public MySqlConnection OpenConnection()
+        {
+            MySqlConnection connection = new MySqlConnection(config);
+            connection.Open();
+            return connection;
+        }
+        public MySqlDataReader Query(string SQL, MySqlConnection connection)
+        {
+            return new MySqlCommand(SQL, connection).ExecuteReader();
+        }
+        public void CloseConnection(MySqlConnection connection)
+        {
+            connection.Close();
+            MySqlConnection.ClearPool(connection);
+        }
+    }
+}
