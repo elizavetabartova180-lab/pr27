a374e6e1a342a5dc31e47ab7c77e961317af3aa7
ADMIN <ADMIN@MSI>
2026-05-07 00:12:24 +0500

    Созданные модели. Бартова

---
+++ Model/Afisha.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kino_Bartova.Model
+{
+    public class Afisha
+    {
+        public int Id { get; set; }
+        public int IdKinoteatr { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public int Price { get; set; }
+        public Afisha(int Id, int IdKinoteatr, string Name, DateTime Time, int Price) { 
+            this.Id = Id;
+            this.IdKinoteatr = IdKinoteatr;
+            this.Name = Name;
+            this.Time = Time;
+            this.Price = Price;
+        }
+
+    }
+}
---
+++ Model/kinoteatr.cs
@@ -0,0 +1,19 @@
+﻿namespace Kino_Bartova.Model
+{
+    public class kinoteatr
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CountZal { get; set; }
+        public int Count { get; set; }
+
+        public kinoteatr(int Id, string Name, int CountZal, int Count)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.CountZal = CountZal;
+            this.Count = Count;
+        }
+
+    }
+}
